f83ca959c7906c4657948138b2288f561b3fc967
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-12-01 11:34:19 +0100

    feat: display with callback books

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.kt
@@ -2,6 +2,14 @@ package fr.android.androidexercises
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+
 
 class LibraryActivity : AppCompatActivity() {
 
@@ -10,19 +18,43 @@ class LibraryActivity : AppCompatActivity() {
         setContentView(R.layout.activity_library)
 
         // Plant logger cf. Android Timber
-        //Timber.plant(Timber.DebugTree())
+        val timber = Timber.plant(Timber.DebugTree())
 
-        // TODO build Retrofit
+        // build Retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://henri-potier.techx.fr")
+            // retrofit est l'orchestrateur
+            // le client HTTP peut être intercepté avec le client suivant
+            //.client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-        // TODO create a service
+        // create a service
+        val service: HenriPotierService = retrofit.create(HenriPotierService::class.java)
 
-        // TODO listBooks()
+        // listBooks()
+        val booksCall: Call<List<Book>> = service.listBooks();
 
-        // TODO enqueue call and display book title
+        // enqueue call and display book title
+        val books = booksCall.enqueue(object : Callback<List<Book>> {
+            override fun onResponse(call: Call<List<Book>>, response: Response<List<Book>>) {
+                Timber.v("${response.body()}")
+                response.body()?.map{
+                    Timber.i(it.title)
+                }
+            }
 
-        // TODO log books
+            override fun onFailure(call: Call<List<Book>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        // log books
+        Timber.v(books.toString())
 
         // TODO display book as a list
+
     }
 
 }
